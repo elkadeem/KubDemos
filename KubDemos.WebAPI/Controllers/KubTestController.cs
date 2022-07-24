6f6e0ba3669695bd25fdc57913ac9dc33c392519
Wael Elkadim <elkadeem@hotmail.com>
2022-07-24 20:23:54 +0200

    Add project files.

---
+++ KubDemos.WebAPI/Controllers/KubTestController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KubDemos.WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KubTestController : ControllerBase
+    {
+        private readonly IConfiguration configuration;
+
+        
+        public KubTestController(IConfiguration configuration)
+        {
+            this.configuration = configuration;
+        }
+
+        [HttpGet]
+        public List<SettingResult> Get()
+        {
+            return configuration.AsEnumerable().Select(c => 
+            new SettingResult { Key = c.Key, Value = c.Value })
+                .ToList();
+        }
+    }
+
+    public class SettingResult
+    {
+        public string Key { get; set; }
+
+        public string Value { get; set; }
+    }
+}
